31e6d73ccbcda02eaf1b2f7ba308007adf3c6e9c
Christopher Guerrero <chrisgrr3@gmail.com>
2020-12-16 23:57:06 -0800

    Created the controller and what will be the methods for the application.

---
+++ authentication/src/main/java/com/chris/main/controllers/Users.java
@@ -0,0 +1,52 @@
+package com.chris.main.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.chris.main.models.User;
+import com.chris.main.services.UserService;
+
+@Controller
+public class Users {
+    private final UserService userService;
+    
+    public Users(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @RequestMapping("/registration")
+    public String registerForm(@ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
+    
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+
+    }
+    
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+        
+    }
+    
+    @RequestMapping("/home")
+    public String home(HttpSession session, Model model) {
+
+    }
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+
+    }
+}
